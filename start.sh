91a6d40aa4a1e2c3c50106a39829e4dacbc35264
Jovansonlee Cesar <ivanceras@gmail.com>
2015-12-31 02:16:02 +0800

    clean up

--- start.sh
+++
@@ -1,2 +0,0 @@
-/home/lee/Dropbox/git-src/ivanceras/curtain/target/release/iron_curtain & 
-/home/lee/Dropbox/git-src/ivanceras/curtain_ui/linux/64bit/nw /home/lee/Dropbox/git-src/ivanceras/curtain_ui/www
